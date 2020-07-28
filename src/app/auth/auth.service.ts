d994940659ce9bd5d6d7a44aaa4339a26be1a3f9
Alec <alecvwilliams@hotmail.com>
2020-07-28 13:35:13 -0700

    Working auth system

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,83 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
+interface SignUpCredentials
+{
+  username: string;
+  password; string;
+  passwordConfirmation: string;
+}
+interface SignUpResponse
+{
+  username: string;
+}
+interface SignedInResponse
+{
+  authenticated: boolean;
+  username: string;
+}
+interface SignInCredentials
+{
+  username: string;
+  password: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private http: HttpClient) { }
+  rootUrl = 'https://api.angular-email.com';
+  signedIn$ = new BehaviorSubject(false); 
+
+  usernameAvailable(username: string)
+  {
+    return this.http.post<{available: boolean}>(this.rootUrl + '/auth/username', {
+      username
+  });
+  }
+
+  signUp(credentials: SignUpCredentials)
+  {
+    return this.http.post<SignUpResponse>(this.rootUrl + '/auth/signup', credentials).pipe(
+      tap(() => 
+      {
+        //chnage to have to sign in after sign up
+        this.signedIn$.next(true);
+      })
+    );
+  }
+
+  checkAuth()
+  {
+    return this.http.get<SignedInResponse>(this.rootUrl + '/auth/signedin').pipe(
+      tap(({ authenticated }) => 
+        {
+          this.signedIn$.next(authenticated);
+      })
+    );
+  }
+
+  signOut()
+  {
+    return this.http.post(this.rootUrl + '/auth/signout', {}).pipe(
+      tap(() => 
+        {
+          this.signedIn$.next(false);
+      })
+    );
+  }
+
+  signIn(credentials: SignInCredentials)
+  {
+    return this.http.post(this.rootUrl + '/auth/signin', credentials).pipe(
+      tap(() => 
+      {
+        this.signedIn$.next(true);
+      })
+    )
+  }
+}
---
+++ src/app/auth/signout/signout.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
+
+
+@Component({
+  selector: 'app-signout',
+  templateUrl: './signout.component.html',
+  styleUrls: ['./signout.component.css']
+})
+export class SignoutComponent implements OnInit {
+
+  constructor(private authService: AuthService, private router: Router) { }
+
+  ngOnInit(): void {
+    this.authService.signOut().subscribe(() => 
+    {
+      //Navigate to signed out page or back to sign in page
+      this.router.navigateByUrl('/');
+    })
+  }
+
+}
